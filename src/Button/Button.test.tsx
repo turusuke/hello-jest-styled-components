97e947eb31bdad6477ba820e294458712d445bdd
turusuke <mail@turusuke.com>
2019-12-31 01:09:04 +0900

    feat: add jest-styled-components

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
+import "jest-styled-components";
 import { render } from "@testing-library/react";
 import Button from "./index";
 
 it("button", () => {
   const { container } = render(<Button>ボタン</Button>);
   expect(container.firstChild).toMatchInlineSnapshot(`
+    .c0 {
+      border: 2px solid tomato;
+    }
+
     <button
-      class="sc-bdVaJa dwVJQt"
+      class="c0"
     >
       ボタン
     </button>
