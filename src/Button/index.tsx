eb68d219689047e90d4a0c9b4708947bb4793fcf
turusuke <mail@turusuke.com>
2019-12-31 01:09:27 +0900

    refactor: prettier

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,6 +1,7 @@
-import React, {FC} from 'react'
-import styled from 'styled-components'
+import React, { FC } from "react";
+import styled from "styled-components";
 
 export default styled.button`
-  border: 2px solid ${({ isError }: { isError?: boolean }) => isError ? 'white' : 'tomato'};
-`
+  border: 2px solid
+    ${({ isError }: { isError?: boolean }) => (isError ? "white" : "tomato")};
+`;
