cd11d3bf6a3b3413bc4d552197bf798b1a8f44b5
turusuke <mail@turusuke.com>
2019-12-31 01:06:06 +0900

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './Button'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Button>hoge</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button/index.tsx
@@ -0,0 +1,6 @@
+import React, {FC} from 'react'
+import styled from 'styled-components'
+
+export default styled.button`
+  border: 2px solid ${({ isError }: { isError?: boolean }) => isError ? 'white' : 'tomato'};
+`
