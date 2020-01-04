bb3b8e23ca7ca0a94d296e792042a2fe65679b28
turusuke <mail@turusuke.com>
2020-01-04 16:43:02 +0900

    fact: toHaveStyleRule の例とネストした styled-component のスタイルチェック例追加

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled, { css } from "styled-components";
 import "@testing-library/jest-dom/extend-expect";
 import "jest-styled-components";
 import { render } from "@testing-library/react";
@@ -33,5 +34,25 @@ describe("button", () => {
         ボタン
       </button>
     `);
+  });
+
+  it("haveStyleRule", () => {
+    const { container } = render(<Button isError={true}>ボタン</Button>);
+    expect(container.firstChild).toHaveStyleRule("border", "2px solid tomato");
+  });
+
+  it("リスト内にあるボタンの border-color がオレンジ色になること", () => {
+    const List = styled.div`
+      ${Button} {
+        border-color: orange;
+      }
+    `;
+
+    const { container } = render(<List><Button /></List>);
+    expect(container.firstChild).toHaveStyleRule("border-color", "orange", {
+      modifier: css`${Button}`
+      // TODO: css 使うと TS でエラーになってしまうので後で調べる
+      // Type 'FlattenSimpleInterpolation' is not assignable to type 'string'.
+    });
   });
 });
